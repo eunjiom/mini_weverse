4c7399e1a2ae5362be789bc30d88a5fd73868970
eunjiom <norgg021@gmail.com>
2026-07-06 00:14:14 +0900

    [chore] Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/miniweverse/MiniWeverseApplication.java
@@ -0,0 +1,13 @@
+package com.miniweverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWeverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniWeverseApplication.class, args);
+    }
+
+}
