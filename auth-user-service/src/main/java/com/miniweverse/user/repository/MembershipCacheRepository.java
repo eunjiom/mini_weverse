1994f157414056bf1f7b296369ce109987c815e5
eunjiom <norgg021@gmail.com>
2026-07-07 13:30:23 +0900

    [feat] 멤버십 도메인 모델에 구독/갱신/취소/만료 로직 추가

--- auth-user-service/src/main/java/com/miniweverse/user/repository/MembershipCacheRepository.java
+++ auth-user-service/src/main/java/com/miniweverse/user/repository/MembershipCacheRepository.java
@@ -1,7 +1,16 @@
 package com.miniweverse.user.repository;
 
 import com.miniweverse.user.entity.MembershipCache;
+import com.miniweverse.user.entity.User;
+import com.miniweverse.user.enums.MembershipStatus;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipCacheRepository extends JpaRepository<MembershipCache, Long> {
+
+    Optional<MembershipCache> findBySubscriberAndArtist(User subscriber, User artist);
+
+    List<MembershipCache> findByStatusAndExpiresAtBefore(MembershipStatus status, LocalDateTime time);
 }
